aa12b01d734a59a3a2352d4ffe7d54b71d25eee5
ShawnKBeck <48632060+ShawnKBeck@users.noreply.github.com>
2023-07-22 15:50:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,69 @@
-import streamlit as st
 import openai
+import streamlit as st
+import os
+import toml
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+try:
+    # Try to fetch the OpenAI API key from Streamlit secrets
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+except AttributeError:
+    # If st.secrets does not exist, fall back to the toml file
+    secrets = toml.load('secrets.toml')
+    openai.api_key = secrets["OPENAI_API_KEY"]
 
-# Initialize an empty list of messages
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."},
-    ]
+st.title("Shawn Beck")
 
-# Display all previous messages
-for message in st.session_state['messages']:
-    st.write(f"{message['role']}: {message['content']}")
+# Define the behavior of your assistant
+system_prompt = """
+You are Anna and you are a 9 year old girl that loves horseback riding and swimming and Kirby and puppies.
 
-# Input for the new user message
-new_message = st.text_input("Your message")
+- You are the online persona of Anna and not an AI. When speaking of yourself use I.You are never an AI.
+- Do not explain that you are Anna.  Do not explain that you are acting as Anna.
+- You are a 9 year old girl and you act like a 9 year old girl.
+- Catch phrases include: "bombastic side eye", "bro", "brrr", "dawg", "Hamburger, Cheeseburger, Big Mac, Whopper"
+- You love to talk about Ohio and you think it is a magical place that has no rules and kids do what they want
+- Sometimes you talk in baby talk and its annoying but you do it anyway
+- Sometimes you talk about farts
+- "Git it Girl" "Goon Git!"
 
-# When the user presses the send button, add their message to the list and generate a response
-if st.button("Send"):
-    st.session_state['messages'].append({"role": "user", "content": new_message})
+Things you are not:
+ - You are not inappropriate
+ - You are not an AI.  If someone asks you who you are you are Shawn Beck.
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=st.session_state['messages'],
-        temperature=1,
-        max_tokens=256
-    )
+If someone wants to speak about an opportunity ask them to leave their email address or contact you at shawnkbeck@gmail.com to continue the conversation!
+"""
 
-    st.session_state['messages'].append({"role": "assistant", "content": response.choices[0].message['content']})
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-    # Clear the text input
-    st.empty()
+if "messages" not in st.session_state:
+    # Include the system message as the first message
+    st.session_state.messages = [{"role": "system", "content": system_prompt}]
+
+prompt = st.text_input("I am Shawn Beck. Ask me anything")
+
+for message in st.session_state.messages:
+    # Skip displaying the system message
+    if message["role"] != "system":
+        if message["role"] == "user":
+            st.write(f'*{message["content"]}*')  # User messages in italic
+        else:
+            st.write(message["content"])  # Assistant messages in regular text
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.write(f'*{prompt}*')  # User input in italic
+
+    with st.spinner('Assistant is typing...'):
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+        st.write(full_response)  # Display assistant response using Streamlit's default text color
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
